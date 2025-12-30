3257def4cb1d7ca5185d336bb62c7c60c995c80f
kazhamiakin <kazhamiakin@gmail.com>
2025-12-30 09:06:22 +0100

    test refactoring

--- test/extproc/run.sh
+++ test/extproc/run.sh
@@ -8,7 +8,7 @@ export PYTHONPATH=$CURRENT_DIR/test/extproc
 echo "PYTHONPATH: $PYTHONPATH"
 
 # set NUCLIO_PYTHON_WRAPPER_PATH
-export NUCLIO_PYTHON_WRAPPER_PATH=$CURRENT_DIR/test/extproc/_nuclio_wrapper.py
+export NUCLIO_PYTHON_WRAPPER_PATH=$CURRENT_DIR/test/python/_nuclio_wrapper.py
 echo "NUCLIO_PYTHON_WRAPPER_PATH: $NUCLIO_PYTHON_WRAPPER_PATH"
 
 go run ./cmd/processor --config=${CURRENT_DIR}/test/extproc/extproc-processor.yaml
