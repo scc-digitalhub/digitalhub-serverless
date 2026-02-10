0468403c95d4f7f7b32374cdbcd7e1af0d16c65f
gosh-a <gglonshakov@gmail.com>
2026-02-10 17:10:28 +0100

    keep progress

--- test/rtsp/test_handler.py
+++ test/rtsp/test_handler.py
@@ -2,15 +2,17 @@ import numpy as np
 import whisper
 import nuclio_sdk
 
+model = whisper.load_model("tiny")
 
 def handler(context: nuclio_sdk.Context, event: nuclio_sdk.Event):
     context.logger.info_with('Invoked rtsp handler')
 
-    model = whisper.load_model("tiny")
+    # model = whisper.load_model("tiny")
     pcm_bytes = event.body
     audio = np.frombuffer(pcm_bytes, dtype=np.int16).astype(np.float32) / 32768.0
 
     result = model.transcribe(audio)
+    # result = model.transcribe(audio, language="de")
     context.logger.info_with(len(pcm_bytes))
     context.logger.info_with(f"transcription: {result['text']}")
 
